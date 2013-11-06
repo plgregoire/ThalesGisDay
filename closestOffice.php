8c7f7a84b654b964489ebdf7b0c35d26fd12e388
Frédéric Morin <fred.morin@gmail.com>
2013-11-06 13:40:31 -0500

    Update closestOffice.php

--- closestOffice.php
+++ closestOffice.php
@@ -18,7 +18,7 @@ $lat = $_GET["lat"];
 $lon = $_GET["lon"];
 select * from calls order by ST_Distance(ST_SetSRID(ST_Point(4,4),4326), geom_p) limit 1;
 
-$request = "http://thalesgisday.cartodb.com/api/v2/sql?format=GeoJSON&q=SELECT%20*%20FROM%20OFFICE%20ORDER%20BY%20ST_DISTANCE(ST_SETSRID(ST_POINT({$lat},{$lon}),4326),geom)%20LIMIT%201";
+$request = "http://thalesgisday.cartodb.com/api/v2/sql?format=GeoJSON&q=SELECT%20*%20FROM%20OFFICES%20ORDER%20BY%20ST_DISTANCE(ST_SETSRID(ST_POINT({$lat},{$lon}),4326),the_geom)%20LIMIT%201";
 $data = file_get_contents($request);
 die($data);
 
