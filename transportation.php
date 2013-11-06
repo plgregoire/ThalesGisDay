cd911fd92d6d5bf61186241608c3a12069f55730
plgregoire <plgregoire81@gmail.com>
2013-11-06 13:22:48 -0500

    Create transportation.php

---
+++ transportation.php
@@ -0,0 +1,15 @@
+<?php
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+header('Content-type: application/json; charset=utf-8');
+
+$request = "http://thalesgisday.cartodb.com/api/v2/sql?format=GeoJSON&q=SELECT%20*%20FROM%20TRANSPORTATION";
+$data = file_get_contents($request);
+die($data);
+
+?>
