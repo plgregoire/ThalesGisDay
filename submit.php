262c27daac00fcc89087f6e218832c607e67f3ae
Frédéric Morin <fred.morin@gmail.com>
2013-11-06 14:00:28 -0500

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,20 @@
+<?php
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+header('Content-type: application/json; charset=utf-8');
+
+$office = $_POST["office"];
+$transportation = $_POST["transportation"];
+
+$apikey = "a63643ebababa18d92daf45dfbb4ebb9d868cecb";
+
+$request = "http://thalesgisday.cartodb.com/api/v2/sql?q=INSERT INTO RESULTS (OFFICE, TRANSPORTATION) VALUES ({$office},{$transportation})&api_key={$apikey}";
+$data = file_get_contents($request);
+die($data);
+
+?>
