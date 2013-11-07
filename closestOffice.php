5fb9220d4ea9a02ac70b74b99366d88cf1580d84
Frédéric Morin <fred.morin@gmail.com>
2013-11-07 12:25:34 -0500

    Update closestOffice.php

--- closestOffice.php
+++ closestOffice.php
@@ -9,13 +9,13 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 header('Content-type: application/json; charset=utf-8');
 
 
-if(!isset($_POST["lat"]) || !isset($_POST["lon"])){
+if(!isset($_GET["lat"]) || !isset($_GET["lon"])){
   // some mandatory parameters are not set.
   die;
 }
 
-$lat = $_POST["lat"];
-$lon = $_POST["lon"];
+$lat = $_GET["lat"];
+$lon = $_GET["lon"];
 if(is_numeric($lat) && is_numeric($lon)){
   $request = "http://thalesgisday.cartodb.com/api/v2/sql?format=GeoJSON&q=SELECT%20*%20FROM%20bureaux_thales%20ORDER%20BY%20ST_DISTANCE(ST_SETSRID(ST_POINT({$lat},{$lon}),4326),the_geom)%20LIMIT%201";
   $data = file_get_contents($request);
