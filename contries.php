3b03d99cc881a04e9a5d87f7c0c65eaeb7560e99
Frédéric Morin <fred.morin@gmail.com>
2013-11-07 12:47:06 -0500

    Update contries.php

--- contries.php
+++ contries.php
@@ -1,4 +1,3 @@
-
 <?php
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
@@ -9,7 +8,7 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 header('Content-type: application/json; charset=utf-8');
 
-$request = "http://thalesgisday.cartodb.com/api/v2/sql?format=GeoJSON&q=SELECT%20tm_world_borders_simpl_0_3.name,%20tm_world_borders_simpl_0_3.the_geom%20FROM%20tm_world_borders_simpl_0_3%20inner%20join%20bureaux_thales%20on%20st_contains(tm_world_borders_simpl_0_3.the_geom,%20bureaux_thales.the_geom)";
+$request = "http://thalesgisday.cartodb.com/api/v2/sql?format=GeoJSON&q=SELECT%20tm_world_borders_simpl_0_3.cartodb_id,tm_world_borders_simpl_0_3.name,%20tm_world_borders_simpl_0_3.the_geom%20FROM%20tm_world_borders_simpl_0_3%20inner%20join%20bureaux_thales%20on%20st_contains(tm_world_borders_simpl_0_3.the_geom,%20bureaux_thales.the_geom)";
 $data = file_get_contents($request);
 die($data);
 
