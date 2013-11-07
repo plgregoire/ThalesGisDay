574bc2e1b3a701952389fbb3286d6b8aa23c19e6
plgregoire <plgregoire81@gmail.com>
2013-11-07 13:15:12 -0500

    prevent history modification by popup

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 		<div id="map"></div>
 
 		<div data-role="content">
-			<div data-role="popup" class="ui-content" data-dismissible="false" id="formPopup" aria-disabled="false" data-disabled="false" data-overlay-theme="a" data-shadow="true" data-corners="true" data-transition="none" data-position-to="window" >
+			<div data-role="popup" class="ui-content" data-dismissible="false" data-history="false" id="formPopup" aria-disabled="false" data-disabled="false" data-overlay-theme="a" data-shadow="true" data-corners="true" data-transition="none" data-position-to="window" >
 				<a href="#" data-rel="back" data-role="button" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Close</a>
 				<form id="commuteform" name="commuteform" method="post" action="index.php" style="line-height:2;">
 					<h4>Select the transportation mode you used to commute today.</h4>
