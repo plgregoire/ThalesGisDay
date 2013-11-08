1075a09a2afd56bcd528a7e208a194ef24b353ef
dions00 <dions00@hotmail.com>
2013-11-08 12:54:00 -0500

    changed office table name and greenfactor

--- cartoDB/CartoDB_GreenessView.sql
+++ cartoDB/CartoDB_GreenessView.sql
@@ -1,9 +1,9 @@
-SELECT bureaux_thales.cartodb_id, bureaux_thales.the_geom_webmercator, COUNT(*) as HitCount, AVG(transportation.green_weight) as weightAverage
+SELECT bureaux_region_country.cartodb_id, bureaux_region_country.the_geom_webmercator, COUNT(*) as CommitCount, AVG(transportation.green_weight) as greenFactorAverage
 FROM results
-INNER JOIN bureaux_thales
+INNER JOIN bureaux_region_country
 ON
-results.office = bureaux_thales.cartodb_id
+results.office = bureaux_region_country.cartodb_id
 INNER JOIN transportation
 ON
 results.transportation = transportation.cartodb_id
-GROUP BY bureaux_thales.cartodb_id, bureaux_thales.the_geom_webmercator
+GROUP BY bureaux_region_country.cartodb_id, bureaux_region_country.the_geom_webmercator
