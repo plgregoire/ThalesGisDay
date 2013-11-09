d52d4dea1e2abc194ceb72d0ebb79db7ae0ef429
plgregoire <plgregoire81@gmail.com>
2013-11-09 09:53:50 -0500

    reduce size of the json returned by office.php

--- office.php
+++ office.php
@@ -11,27 +11,13 @@ header('Content-type: application/json; charset=utf-8');
 
 if(isset($_GET["country_id"]) && is_numeric($_GET["country_id"])){
   $id = $_GET["country_id"];
-  $getData = http_build_query(
-				array(
-					'format' => 'GeoJSON',
-					'q' => "SELECT bureaux_region_country.cartodb_id,bureaux_region_country.the_geom, bureaux_region_country.name FROM bureaux_region_country inner join tm_world_borders_simpl_0_3 on st_contains(tm_world_borders_simpl_0_3.the_geom, bureaux_region_country.the_geom) where tm_world_borders_simpl_0_3.cartodb_id =".$id
-				));
-  
-  $request = "http://thalesgisday.cartodb.com/api/v2/sql?".$getData;
-  
-  $data = file_get_contents($request);
-  die($data);
+  $request = "http://thalesgisday.cartodb.com/api/v2/sql?q=SELECT%20bureaux_region_country.cartodb_id%2C%20bureaux_region_country.name%20FROM%20bureaux_region_country%20inner%20join%20tm_world_borders_simpl_0_3%20on%20st_contains(tm_world_borders_simpl_0_3.the_geom%2C%20bureaux_region_country.the_geom)%20where%20tm_world_borders_simpl_0_3.cartodb_id%20%3D".$id;
 }else{
-  $getData = http_build_query(
-				array(
-					'format' => 'GeoJSON',
-					'q' => 'SELECT * FROM bureaux_region_country'
-				)
-			);
-  $request = "http://thalesgisday.cartodb.com/api/v2/sql?".$getData;
+  $request = "http://thalesgisday.cartodb.com/api/v2/sql?q=SELECT%20bureaux_region_country.cartodb_id%2C%20bureaux_region_country.name%20FROM%20bureaux_region_country";
+}
+
   $data = file_get_contents($request);
   die($data);
-}
 
 
 ?>
