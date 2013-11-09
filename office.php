b5cdf20edfa93ae1f5ddcb30b964124d724038d6
plgregoire <plgregoire81@gmail.com>
2013-11-08 20:45:38 -0500

    use the master version of leaflet (grey tiles on mobile devices)

--- office.php
+++ office.php
@@ -14,7 +14,7 @@ if(isset($_GET["country_id"]) && is_numeric($_GET["country_id"])){
   $getData = http_build_query(
 				array(
 					'format' => 'GeoJSON',
-					'q' => "SELECT bureaux_region_country.cartodb_id,bureaux_region_country.the_geom, bureaux_region_country.name FROM bureaux_region_country inner join tm_world_borders_simpl_0_3 on st_contains(tm_world_borders_simpl_0_3.the_geom, bureaux_region_country.the_geom) where tm_world_borders_simpl_0_3.cartodb_id =".$id
+					'q' => "SELECT bureaux_region_country.cartodb_id,bureaux_region_country.name FROM bureaux_region_country inner join tm_world_borders_simpl_0_3 on st_contains(tm_world_borders_simpl_0_3.the_geom, bureaux_region_country.the_geom) where tm_world_borders_simpl_0_3.cartodb_id =".$id
 				));
   
   $request = "http://thalesgisday.cartodb.com/api/v2/sql?".$getData;
@@ -25,7 +25,7 @@ if(isset($_GET["country_id"]) && is_numeric($_GET["country_id"])){
   $getData = http_build_query(
 				array(
 					'format' => 'GeoJSON',
-					'q' => 'SELECT * FROM bureaux_region_country'
+					'q' => 'SELECT bureaux_region_country.cartodb_id,bureaux_region_country.name FROM bureaux_region_country'
 				)
 			);
   $request = "http://thalesgisday.cartodb.com/api/v2/sql?".$getData;
