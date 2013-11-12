959c4705f95bf0c27eaa2c87f081c7a56f2c88a1
perrond <dominique.perron@ca.thalesgroup.com>
2013-11-12 12:22:13 -0500

    changed API key

--- submit.php
+++ submit.php
@@ -13,7 +13,7 @@ $lon = $_POST["lon"];
 $office = $_POST["office"];
 $transportation = $_POST["transportation"];
 if(is_numeric($lat) && is_numeric($lon) && is_numeric($office) && is_numeric($transportation)){
-  $apikey = "a63643ebababa18d92daf45dfbb4ebb9d868cecb";
+  $apikey = "5a6d29fb178dbdd1a1cd394b7a85102e3258872c";
   $ipaddress = getenv('REMOTE_ADDR');
   $proxyIPAddress = getenv('HTTP_X_FORWARDED_FOR');
   $request = "http://thalesgisday.cartodb.com/api/v2/sql?q=INSERT%20INTO%20RESULTS%20(OFFICE,%20TRANSPORTATION,%20LATITUDE,%20LONGITUDE,%20IPADDRESS,%20PROXYIPADDRESS)%20VALUES%20({$office},{$transportation},{$lat},{$lon},'{$ipaddress}','{$proxyIPAddress}')&api_key={$apikey}";
