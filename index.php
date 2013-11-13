c550c331df8814e2410b2e96cac5dad033897ce3
jsthivierge <jeansebastien.thivierge@gmail.com>
2013-11-12 21:00:45 -0500

    Prevent zooming on mobile devices

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title></title>
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
         <link rel="stylesheet" href="css/normalize.min.css">
         
