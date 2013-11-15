773d7f67f7f6dfb03c93227f705b71ef34f97eb6
jsthivierge <jeansebastien.thivierge@gmail.com>
2013-11-15 12:58:52 -0500

    New API Key

--- submit.php
+++ submit.php
@@ -13,7 +13,7 @@ $lon = $_POST["lon"];
 $office = $_POST["office"];
 $transportation = $_POST["transportation"];
 if(is_numeric($lat) && is_numeric($lon) && is_numeric($office) && is_numeric($transportation)){
-  $apikey = "3cb290cbfbca55ec6170ac319531238a97ae42a0";
+  $apikey = "35a587ad90bc9a1c4fe97c71337d0593462c00fd";
   $ipaddress = getenv('REMOTE_ADDR');
   $proxyIPAddress = getenv('HTTP_X_FORWARDED_FOR');
   $request = "http://gisday2013atthales.cartodb.com/api/v2/sql?q=INSERT%20INTO%20RESULTS%20(the_geom,OFFICE,TRANSPORTATION,LATITUDE,LONGITUDE,IPADDRESS,PROXYIPADDRESS)%20VALUES(ST_SetSRID(ST_Point({$lon},{$lat}),4326),{$office},{$transportation},{$lat},{$lon},'{$ipaddress}','{$proxyIPAddress}')&api_key={$apikey}";
